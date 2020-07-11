db88649cea3e39a09c8dc5f304a0fb6572de65b8
Phung-Tiep <phungtsm70@wru.vn>
2020-07-11 17:17:17 +0700

    Wrap up getting started

---
+++ PTchat/Controllers/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  PTchat
+//
+//  Created by Tiếp Sỹ Minh Phụng on 7/11/20.
+//  Copyright © 2020 PTgroup. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PTchat/Controllers/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  PTchat
+//
+//  Created by Tiếp Sỹ Minh Phụng on 7/11/20.
+//  Copyright © 2020 PTgroup. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
