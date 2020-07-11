5442ad3236f50234ba0cff5f8f733b3db92d7511
Phung-Tiep <phungtsm70@wru.vn>
2020-07-11 17:01:52 +0700

    Getting Started

---
+++ PTchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PTchat
+//
+//  Created by Tiếp Sỹ Minh Phụng on 7/11/20.
+//  Copyright © 2020 PTgroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
